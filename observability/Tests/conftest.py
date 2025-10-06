78af048138760368b92292757268e1312e4d594b
mahesh <mail2mahebabu@gmail.com>
2025-10-06 19:55:41 +0530

    change

--- observability/Tests/conftest.py
+++ observability/Tests/conftest.py
@@ -149,7 +149,7 @@ def server():
     option.add_argument("--disable-notifications")
     # option.add_argument("--headless=new")
     option.add_argument("--no-sandbox")
-    option.add_argument("--disable-dev-shm-usage")
+    # option.add_argument("--disable-dev-shm-usage")
     option.add_argument("--allow-running-insecure-content")
     option.add_argument("--disable-web-security")
 
